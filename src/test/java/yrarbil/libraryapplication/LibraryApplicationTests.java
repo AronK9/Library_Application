cc4e67cd4703e5d12c2247d2116e2e5f64093189
AronK9 <128731298+AronK9@users.noreply.github.com>
2024-02-09 16:57:40 +0100

    Implemented entities and enums

--- src/test/java/yrarbil/libraryapplication/LibraryApplicationTests.java
+++ src/test/java/yrarbil/libraryapplication/LibraryApplicationTests.java
@@ -1,9 +1,7 @@
 package yrarbil.libraryapplication;
 
 import org.junit.jupiter.api.Test;
-import org.springframework.boot.test.context.SpringBootTest;
 
-@SpringBootTest
 class LibraryApplicationTests {
 
     @Test
